f35b18795f50e6aa8012e3e1eab2d550ba829275
Jonah Troublemaker Yang <Jonah@HeSaid.Love>
2025-01-28 03:54:11 +0000

    Add venv setup to install script

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -4,3 +4,8 @@ sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
 sudo apt-get install redis
+
+sudo apt install python3-venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
